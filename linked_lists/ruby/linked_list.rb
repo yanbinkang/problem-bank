283acb5d100e5f3c866f1409e5168d653e73a357
Albert Agram <albert.agram@gmail.com>
2016-11-10 11:15:32 +0000

    simple node class for linked lists

--- linked_lists/ruby/linked_list.rb
+++ linked_lists/ruby/linked_list.rb
@@ -1,12 +1,12 @@
 class Node
   attr_accessor :data, :next_pointer
 
-  def initialize
-    @data = nil
+  def initialize(init_data)
+    @data = init_data
     @next_pointer = nil
   end
 
   def has_next?
-    self.next_pointer != nil
+    !self.next_pointer.nil?
   end
 end
