b37f0e4ba2f28e0c6c356986f97f2143f49eb28c
Albert Agram <albert.agram@gmail.com>
2018-01-04 17:34:10 +0000

    change intersection 2 copy

--- leetcode/hash_table/python/intersection_2_leetcode.py
+++ leetcode/hash_table/python/intersection_2_leetcode.py
@@ -6,7 +6,7 @@ Given two arrays, write a function to compute their intersection.
 Example:
 Given nums1 = [1, 2, 2, 1], nums2 = [2, 2], return [2, 2].
 
-Note: Intersection means elements common to the two arrays
+Note: Intersection means elements common to both arrays
 
 Note:
     * Each element in the result should appear as many times as it shows in both arrays.
