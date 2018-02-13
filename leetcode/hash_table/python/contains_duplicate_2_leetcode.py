5357aeb2520107f33eb0c0b748d940b1d703b9ae
Albert Agram <albert.agram@gmail.com>
2018-02-13 11:51:21 +0000

    add question description

--- leetcode/hash_table/python/contains_duplicate_2_leetcode.py
+++ leetcode/hash_table/python/contains_duplicate_2_leetcode.py
@@ -1,7 +1,7 @@
 """
-Given an array of integers and an integer k, find out whether there are two distinct indices i and j in the array such that nums[i] = nums[j] and the absolute difference between i and j is at most k.
-
 https://leetcode.com/problems/contains-duplicate-ii/
+
+Given an array of integers and an integer k, find out whether there are two distinct indices i and j in the array such that nums[i] = nums[j] and the absolute difference between i and j is at most k.
 
 https://discuss.leetcode.com/topic/22444/python-concise-solution-with-dictionary
 """
