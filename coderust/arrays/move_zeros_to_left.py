ca87696e16f1b9bd147db580b56360f90a4da039
Albert Agram <albert.agram@gmail.com>
2018-03-06 17:54:18 +0000

    move_zeros_to_left

--- coderust/arrays/move_zeros_to_left.py
+++ coderust/arrays/move_zeros_to_left.py
@@ -1,39 +1,15 @@
 def move_zeros_to_left(a_list):
-    move_zeros_helper(a_list, 0, len(a_list)-1)
+    insert_pos = len(a_list) - 1
 
-def move_zeros_helper(a_list, first, last):
-    if first < last:
-        split_point = partition(a_list, first, last)
-        move_zeros_helper(a_list, first, split_point-1)
-        move_zeros_helper(a_list, split_point+1, last)
+    for i in reversed(range(len(a_list))):
+        if a_list[i] != 0:
+            a_list[insert_pos] = a_list[i]
+            insert_pos -= 1
 
-def partition(a_list, first, last):
-    pivot_value = a_list[first]
-    left_mark = first + 1
-    right_mark = last
+    while insert_pos >= 0:
+        a_list[insert_pos] = 0
+        insert_pos -= 1
 
-    stop = False
+    return a_list
 
-    while not stop:
-        while left_mark <= right_mark and a_list[left_mark] <= pivot_value:
-            left_mark += 1
-
-        while right_mark >= left_mark and a_list[right_mark] >= pivot_value:
-            right_mark -= 1
-
-        if right_mark < left_mark:
-            stop = True
-        else:
-            temp = a_list[left_mark]
-            a_list[left_mark] = a_list[right_mark]
-            a_list[right_mark] = temp
-
-    temp = a_list[first]
-    a_list[first] = a_list[right_mark]
-    a_list[right_mark] = temp
-
-    return right_mark
-
-my_list = [1, 10, 20, 0, 59, 63, 0, 88, 0]
-move_zeros_to_left(my_list)
-print my_list
+print move_zeros_to_left([1, 10, 20, 0, 59, 63, 0, 88, 0])
